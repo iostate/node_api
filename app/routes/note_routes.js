c77f382435bc879141386a3f3cdff5586b28065c
Quan Truong <qmtruong92@gmail.com>
2018-05-12 15:45:21 -0700

    Full node API working

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,15 +1,51 @@
-// Routes are wrapped in a function. 
+// In order to retrieve a note with an ID, we must declare an 
+// ObjectID from mongodb
+var ObjectID = require('mongodb').ObjectID;
 
+// Routes are wrapped in a function. 
+// const db = db.collection('notes');
 // Takes express instance and database as argument
 module.exports = function(app, db) {
+	// Test URL for get request: localhost:8000/notes/5af6f86b59554e601023d4e9
+	// Get request for a particular note
+	app.get('/notes/:id', (req, res) => {
+		// receive ID from GET request in URL
+		const id = req.params.id;
+		const details = { '_id': new ObjectID(id)};
+		db.collection('notes').findOne(details, (err, item) => {
+			if (err) {
+				res.send({'error': 'An error has occurred during your GET request'});
+			} else {
+				console.log(item);
+				res.send(item);
+			}
+		});
+	});
+	
 	// Create a POST request in order to create a note
 	app.post('/notes', (req, res) => {
+		const note = { text: req.body.body, title: req.body.title };
+		db.collection('notes').insert(note, (err, result) => {
+			if (err) {
+				res.send({ 'error': 'An error has occurred'});
+			} else {
+				console.log(result.ops[0]);
+				res.send(result.ops[0]);
+			}
+		});
+	});
 
-		//log the body of the response
-		console.log(req.body)
-
-		// Response will be 'hello'
-		res.send('Hello')
+	// DELETE REQUEST
+	app.delete('/notes/:id', (req, res) => {
+		const id = req.params.id;
+		const details = { '_id': new ObjectID(id) };
+		db.collection('notes').remove(details, (err, item) => {
+			if (err) {
+				res.send({'error': 'An error has occurred'});
+			} else {
+				res.send('Note ' + id + ' deleted');
+			}
+		});
 	});
 };
 // You can now export this through index.js
--- server.js
+++ server.js
@@ -6,12 +6,23 @@ const db = require('./config/db');
 const app = express();
 
 const port = 8000;
+
+// Allows response's to not be undefined?? Look into what this does later
 app.use(bodyParser.urlencoded({extended: true}));
 
-// import the routes for use here in server.js 
-// before listening to the port
-require('./app/routes')(app, {}); // no DB yet so we pass in an empty object
-app.listen(port, () => {
-	console.log('Now live on port ' + port);
-});
+//Wrap the app configuration with the MongoClient
+// Unsure what the new URL parser is.. 
+MongoClient.connect(db.url, {useNewUrlParser: true}, (err, database) => {
+	if (err) return console.log(err)
 
+	mdb = database.db("infostore")
+	// Make sure to add the database name and not collection name
+	// db = database.db("infostore")
+
+	// import the routes for use here in server.js 
+	// before listening to the port
+	require('./app/routes')(app, mdb); // no DB yet so we pass in an empty object
+	app.listen(port, () => {
+		console.log('Now live on port ' + port);
+	});	
+})
